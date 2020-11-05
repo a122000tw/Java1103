d8acaef732b82abcafe392b5b43f8d36b1b459f4
MB-study <MB-study@192.168.1.209>
2020-11-05 21:33:02 +0800

    1103

--- src/main/java/com/ocp/PrintStars_HW.java
+++ src/main/java/com/ocp/PrintStars_HW.java
@@ -3,21 +3,15 @@ package com.ocp;
 public class PrintStars_HW {
 
     public static void main(String[] args) {
-        int line = 7;
-        for (int k = 6; k >= 0; k--) {
-            for (int m = 1; m >= 0; m--) {
 
-                System.out.print("5");
+        for (int i = 1; i <= 6; i++) {
+            for (int k = 6 - i; k > 0; k--) {
+                System.out.print(" ");
             }
-
-        }
-        for (int i = 0; i < line; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
-
