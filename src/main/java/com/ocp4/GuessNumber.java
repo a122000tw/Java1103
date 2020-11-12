8eb4bcda0535cdbf72d39fbd1941e22c3af420e7
MB-study <MB-study@192.168.1.209>
2020-11-12 21:44:18 +0800

    1103

--- src/main/java/com/ocp4/GuessNumber.java
+++ src/main/java/com/ocp4/GuessNumber.java
@@ -11,9 +11,9 @@ public class GuessNumber {
         do {
             Scanner sc = new Scanner(System.in);
             System.out.println("請輸入四位數: ");
-            String input = sc.next(); // 變成[1, 2, 3, 4]
+            String input = sc.next(); 
             int[] guess = {input.charAt(0) - 48, input.charAt(1) - 48, input.charAt(2) - 48, input.charAt(3) - 48};
-            System.out.println(Arrays.toString(guess));
+            System.out.println(Arrays.toString(guess)); // 變成[1, 2, 3, 4]
             // 先比 B (數字相同)
             int b = 0;
             for (int i = 0; i < guess.length; i++) {
