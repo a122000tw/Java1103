6ec21e432f6d3b16b42a4585c527f0362673df67
MB-study <MB-study@192.168.1.209>
2020-11-12 20:09:48 +0800

    1103

--- src/main/java/com/ocp4/ArrayDemo.java
+++ src/main/java/com/ocp4/ArrayDemo.java
@@ -20,6 +20,27 @@ public class ArrayDemo {
         // 算出體重平均
         double w_avg = getAvg(weight);
         System.out.printf("身高平均:%.1f, 體重平均:%.1f\n", h_avg, w_avg);
+        // 算出BMI平均
+        double bmi_avg = getBMIAvg(height, weight);
+        System.out.printf("BMI平均:%.2f\n", bmi_avg);
+    }
+        
+
+    public static double getBMIAvg(double[] h_array, double[] w_array) {
+        if (h_array.length != w_array.length) {
+            System.out.println("資料長度不一致");
+            return 0.0;
+        }
+        double bmi_sum = 0; // bmi的總和
+        int len = h_array.length; // 先把長度存入len的變數中
+        //因為 h_array 與 w_array的長度相同，可共用i值
+        for (int i = 0; i < h_array.length; i++) {
+            double h = h_array[i];
+            double w = w_array[i];
+            double bmi = w / Math.pow(h / 100, 2);
+            bmi_sum += bmi; // 累計bmi的總和
+        }
+        return bmi_sum / len; // 計算bmi的平均
     }
 
     public static double getSum(double[] array) {
