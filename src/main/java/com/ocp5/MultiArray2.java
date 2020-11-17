941472ec8b2731e153b97f2899aca7a60d999c9c
MB-study <MB-study@192.168.1.209>
2020-11-17 21:18:08 +0800

    1103

--- src/main/java/com/ocp5/MultiArray2.java
+++ src/main/java/com/ocp5/MultiArray2.java
@@ -2,6 +2,8 @@ package com.ocp5;
 
 import java.util.Arrays;
 import static com.utils.MathUtil.avg;
+import static com.utils.MathUtil.sd;
+import static com.utils.MathUtil.cv;
 
 /*
 某公司有19位員工，其中10位在去年投資股票，一年的獲
@@ -13,17 +15,20 @@ import static com.utils.MathUtil.avg;
 試分別求此公司的員工投資股票與公債的獲利率變異係數。
  */
 public class MultiArray2 {
-
+    
     public static void main(String[] args) {
         double[][] profit = {
             {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5},
             {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2}
         };
         for (int i = 0; i < profit.length; i++) {
-            System.out.printf("%s 平均: %.1f\n", Arrays.toString(profit[i]),avg(profit[i]));
+            System.out.printf("%s 平均: %.1f\n",
+                    Arrays.toString(profit[i]),
+                    avg(profit[i]),
+                    sd(profit[i]),
+                    cv(profit[i])
+            );
         }
     }
-    
-    
     
 }
--- src/main/java/com/utils/MathUtil.java
+++ src/main/java/com/utils/MathUtil.java
@@ -1,7 +1,7 @@
 package com.utils;
 
 public class MathUtil {
-
+    // 總和
     public static double sum(double[] values) {
         int sum = 0;
         for (int i = 0; i < values.length; i++) {
@@ -11,8 +11,28 @@ public class MathUtil {
 
         return sum;
     }
-
+    // 平均
     public static double avg(double[] values) {
         return sum(values) / values.length;
+    }
+    // 標準差
+    public static double sd(double[] values) {
+        // 身高平方差的總和
+        double avg = avg(values);
+        double h_diff_sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            h_diff_sum += Math.pow(values[i] - avg, 2);
+        }
+        System.out.printf("身高平方差的總和: %.2f\n", h_diff_sum);
+        // 身高的標準差(SD)
+        double sd = Math.sqrt((1.0 / values.length) * h_diff_sum);
+        return sd;
+    }
+    // 變異係數
+    public static double cv(double [] values) {
+        double sd = sd(values);
+        double avg = avg(values);
+        double cv = sd / avg;
+        return cv;
     }
 }
