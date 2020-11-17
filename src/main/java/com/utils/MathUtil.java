2233a4f8dd963555a423643ce4820b0a5df57362
MB-study <MB-study@192.168.1.209>
2020-11-17 21:32:32 +0800

    1103

--- src/main/java/com/utils/MathUtil.java
+++ src/main/java/com/utils/MathUtil.java
@@ -1,6 +1,7 @@
 package com.utils;
 
 public class MathUtil {
+
     // 總和
     public static double sum(double[] values) {
         int sum = 0;
@@ -11,10 +12,12 @@ public class MathUtil {
 
         return sum;
     }
+
     // 平均
     public static double avg(double[] values) {
         return sum(values) / values.length;
     }
+
     // 標準差
     public static double sd(double[] values) {
         // 身高平方差的總和
@@ -23,16 +26,40 @@ public class MathUtil {
         for (int i = 0; i < values.length; i++) {
             h_diff_sum += Math.pow(values[i] - avg, 2);
         }
-        System.out.printf("身高平方差的總和: %.2f\n", h_diff_sum);
+
         // 身高的標準差(SD)
         double sd = Math.sqrt((1.0 / values.length) * h_diff_sum);
         return sd;
     }
+
     // 變異係數
-    public static double cv(double [] values) {
+    public static double cv(double[] values) {
         double sd = sd(values);
         double avg = avg(values);
         double cv = sd / avg;
         return cv;
     }
+
+    // 取得最大值  
+    public static double max(double[] values) {
+        double max = Integer.MIN_VALUE;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > max) {
+                max = values[i];
+            }
+        }
+        return max;
+    }
+
+    //取得最小值
+    public static double min(double[] values) {
+        double min = Integer.MAX_VALUE;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] < min) {
+                min = values[i];
+            }
+        }
+        return min;
+    }
+
 }
