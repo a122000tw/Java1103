a21d751114d660eccb12ca06d0d3af78bf9a4cd4
MB-study <MB-study@192.168.1.209>
2020-12-03 21:31:45 +0800

    1103

--- src/main/java/com/ocp10/pet/PetStore.java
+++ src/main/java/com/ocp10/pet/PetStore.java
@@ -1,8 +1,28 @@
 
 package com.ocp10.pet;
 
+import com.ocp10.pet.service.PetService;
+import com.ocp10.pet.entity.Chihuahua;
+import com.ocp10.pet.entity.EDog;
+import com.ocp10.pet.entity.Pet;
+import com.ocp10.pet.entity.Shiba;
+
+
 public class PetStore {
     public static void main(String[] args) {
+        PetService service = new PetService();
+        Pet[] pets = service.getPets();
+        service.print(pets);
+        int totalPrice = service.allPrice(pets);
+        System.out.printf("Total price: %,d\n", totalPrice);
+        int totalPriceByShiba = service.getPriceByShiba(pets);
+        System.out.printf("Total price: %,d\n", totalPriceByShiba);
+        //--------------------------------------------------------------
+        // 印出每一個寵物的總價
+        System.out.println("\n印出每一個寵物的總價");
+        service.printEachTotalPriceByPet(pets);
+        
+        
         
     }
 }
