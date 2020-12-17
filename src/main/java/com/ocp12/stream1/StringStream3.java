3b1d2e0d3ea1ca02e88f53fe4b1f67d7b3797b62
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-17 21:39:37 +0800

    Java

---
+++ src/main/java/com/ocp12/stream1/StringStream3.java
@@ -0,0 +1,19 @@
+
+package com.ocp12.stream1;
+
+import java.util.stream.Stream;
+
+public class StringStream3 {
+    public static void main(String[] args) {
+        String[] names = {"Java", "Python", "C", "Visual Basic"};
+        // 印出每個字串長度的總和
+        // {4, 6, 1, 12} = 23
+        int sum = Stream.of(names).mapToInt(x -> x.length()).sum();
+        System.out.println(sum);
+        // 印出長度 <=4 的長度各是多少
+        Stream.of(names).mapToInt(x -> x.length()).filter(x -> x <= 4).forEach(x -> System.out.println(x));
+        Stream.of(names).mapToInt(String::length).filter(Util::isLessThan4).forEach(System.out::println);
+    }
+    
+    
+}
