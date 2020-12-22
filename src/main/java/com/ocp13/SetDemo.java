5b239d13f0c41e8008248a14ceb54aea4045a6bb
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-22 19:20:21 +0800

    Java

---
+++ src/main/java/com/ocp13/SetDemo.java
@@ -0,0 +1,27 @@
+
+package com.ocp13;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        Set set = new HashSet(); // 特性:元素之間的擺放是按照HashCode來決定，提高元素查詢的效率
+        set.add("國文"); // String
+        set.add(100); // 不是int而是Integer
+        set.add("英文"); // String
+        set.add(Integer.valueOf("100")); // Integer(重複)
+        set.add("數學"); // String
+        set.add(new Integer("90")); // Integer
+        System.out.println(set);
+        // for-loop
+        for(Object obj : set) {
+            System.out.println(obj);
+        }
+        // Java 8 forEach
+        set.forEach(e -> System.out.println(e));
+        set.forEach(System.out::println);
+    }
+}
