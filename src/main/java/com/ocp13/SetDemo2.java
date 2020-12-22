14828225854aabf059d5080b116f8cf8f63f76d0
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-22 19:46:04 +0800

    Java

--- src/main/java/com/ocp13/SetDemo2.java
+++ src/main/java/com/ocp13/SetDemo2.java
@@ -2,12 +2,14 @@
 package com.ocp13;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetDemo2 {
     public static void main(String[] args) {
-        Set set = new HashSet();
+        Set set = new TreeSet(); //LinkedHashSet(); //HashSet();
         // 今彩539, 1~39中取5個不重複的數字
         // 電腦選號
         while (set.size() < 5) {            
@@ -17,6 +19,19 @@ public class SetDemo2 {
             System.out.println("加入 ==> " + n);
         }
         System.out.println(set);
-        
+        // 彩球的號碼總和?
+        // Java for-loop
+        int sum = 0;
+        for(Object obj : set) {
+           if(obj instanceof Integer) {
+               sum += (Integer)obj;
+           }
+        }
+        System.out.println(sum);
+        // Java 8 (Object轉型為int)
+        int sum2 = set.stream().mapToInt(e -> (Integer)e).sum(); // Java 5 後可以省略intValue的調用
+        int sum3 = set.stream().mapToInt(e -> ((Integer)e).intValue()).sum();
+        System.out.println(sum2);
+        System.out.println(sum3);
     }    
 }
