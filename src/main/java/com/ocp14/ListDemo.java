4cb0e14d08f50ab8d2b164a1620e825da825ecd5
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-24 19:59:59 +0800

    Java

---
+++ src/main/java/com/ocp14/ListDemo.java
@@ -0,0 +1,34 @@
+
+package com.ocp14;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> scores = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            scores.add(new Random().nextInt(11)); // 0~10
+        }
+        System.out.println(scores);
+        // 排序
+        Collections.sort(scores);
+        System.out.println(scores);
+        // 去掉最前面2個元素
+        scores.remove(0); // 去掉位置0的元素
+        scores.remove(0); // 再去掉位置0的元素
+        System.out.println(scores);
+        // 去掉最後面2個元素
+        scores.remove(scores.size()-1); // 去掉最後一個元素
+        scores.remove(scores.size()-1); // 去掉最後一個元素
+        System.out.println(scores);
+        double avg = scores.stream()
+                .mapToInt(score -> score) // score -> score.intValue()
+                .average()
+                .getAsDouble();
+        System.out.println(avg);
+    }
+}
---
+++ src/main/java/com/ocp14/QueueDemo.java
@@ -0,0 +1,46 @@
+
+package com.ocp14;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> pokers = new LinkedList<>(); 
+        for (int i = 1; i < 10; i++) pokers.add(i+"");  
+        pokers.add("J");pokers.add("Q");pokers.add("K");
+        System.out.println(pokers);
+        // 洗牌? 調換位置
+        Collections.shuffle((List)pokers);
+        System.out.println(pokers);
+        String card1 = pokers.poll();
+        String card2 = pokers.poll();
+        System.out.println(card1);
+        System.out.println(card2);
+        // 計算分數
+        double score = getScore(card1, card2);
+        // double score = getScore(card1) + getScore(card2);        
+        // 剩餘的牌
+        System.out.println(pokers);
+        
+    }
+    
+    
+    
+    public static double getScore(String card1, String card2) {
+        return getScore(card1) + getScore(card2);
+    }
+    
+    
+    public static double getScore(String card) {
+        if("JQK".contains(card)) {
+            return 0.5;
+        } else {
+            return Double.parseDouble(card); // 將字串轉為double
+        }
+    }
+    
+    
+}
