17d419ef47e90b5738925af9eb9f8f36f56a4c32
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-05 20:30:46 +0800

    Java

---
+++ src/main/java/com/ocp16/ExceptionDemo3.java
@@ -0,0 +1,10 @@
+
+package com.ocp16;
+
+import java.io.File;
+
+public class ExceptionDemo3 {
+    public static void main(String[] args) {
+        File file = new File("src\\main\\java\\com\\ocp16\\number.txt");
+    }
+}
