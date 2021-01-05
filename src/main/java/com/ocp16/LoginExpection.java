5dece55ab1942ec57589dcc6938ee7ff8b5aca54
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-05 21:39:13 +0800

    Java

---
+++ src/main/java/com/ocp16/LoginExpection.java
@@ -0,0 +1,17 @@
+
+package com.ocp16;
+
+public class LoginExpection extends Exception{
+    
+    public LoginExpection(String string) {
+        // 將錯誤訊息傳給父類別
+        super();
+    }
+    
+    
+    // 自訂一個新方法
+    public void 怎麼辦() {
+        System.out.println("請重新登入");
+    }
+    
+}
---
+++ src/main/java/com/ocp16/LoginSystem2.java
@@ -0,0 +1,30 @@
+
+package com.ocp16;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginSystem2 {
+    // 可以登入的使用者
+    private static List<String> users = Arrays.asList("john", "mary", "admin");
+    
+    public static boolean login(String userName) throws LoginExpection{
+        boolean check = users.stream().filter(name -> name.equals(userName)).findFirst().isPresent();
+        if(check == false) { // if(! check)
+            LoginExpection le = new LoginExpection("無此帳號");
+            throw le;
+        }
+        return check;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            System.out.println(login("apple"));
+        } catch (LoginExpection ex) {
+            System.out.println(ex.getMessage());
+            ex.怎麼辦();
+        }
+        
+        
+    }
+}
