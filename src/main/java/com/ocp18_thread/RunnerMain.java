eafd3351cdc52ebba7fdb234adb5f6769ec39c8e
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-14 21:04:50 +0800

    Java

--- src/main/java/com/ocp18_thread/RunnerMain.java
+++ src/main/java/com/ocp18_thread/RunnerMain.java
@@ -7,6 +7,8 @@ public class RunnerMain {
         Runner r2 = new Runner();
         r1.setName("烏龜");
         r2.setName("兔子");
+        // 背景執行續
+        r2.setDaemon(true);
         r1.setPriority(Thread.MAX_PRIORITY); // 1~10 max(10)
         r2.setPriority(Thread.MIN_PRIORITY); // 1~10 min(1)
         r1.start(); // 啟動執行續
---
+++ src/main/java/com/ocp19_thread/Shower.java
@@ -0,0 +1,54 @@
+
+package com.ocp19_thread;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Father extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("爸爸下班回家");
+        System.out.println("爸爸準備洗澡");
+        System.out.println("爸爸發現沒瓦斯");
+        System.out.println("爸爸打電話叫瓦斯");
+        Worker worker = new Worker();
+        worker.start();
+        try {
+            // 暫停的方法
+            worker.join(10_000);
+        } catch (InterruptedException ex) {
+            System.out.println("join失敗");
+        }
+        System.out.println("爸爸開始洗澡");
+        System.out.println("爸爸洗完澡了");
+    }
+    
+}
+class Worker extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("工人送瓦斯中...");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + "秒鐘  ");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) { // 中斷意外
+                System.out.println("瓦斯工人發生意外");
+                return;
+            }
+        }
+        System.out.println();
+        System.out.println("工人將瓦斯送到");
+        
+    }
+    
+}
+
+public class Shower {
+    public static void main(String[] args) {
+        Father father = new Father();
+        father.start();
+    }
+}
