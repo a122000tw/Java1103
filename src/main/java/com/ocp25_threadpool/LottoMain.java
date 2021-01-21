2cc9c34be9e9ada69a1009660fbb92704c8f36b2
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-21 21:19:00 +0800

    Java

---
+++ src/main/java/com/ocp25_threadpool/LottoMain.java
@@ -0,0 +1,18 @@
+
+package com.ocp25_threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class LottoMain {
+    public static void main(String[] args) throws Exception{
+        // ExecutorService exec = Executors.newCachedThreadPool();
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+             exec.submit(new Lotto());
+            System.out.println("執行續使用量: " + Thread.activeCount());
+        }
+       exec.shutdown();
+    }
+}
